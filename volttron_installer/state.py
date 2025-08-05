7a4cf1b8ad20a95ce6788dbad3bdb2720e331410
dominrios <dominic.rios@pnnl.gov>
2025-08-05 12:16:20 -0700

    #124 - Created a check and disabled saving if the host_configs_dir matches another VOLTTRON under the same host.

--- volttron_installer/state.py
+++ volttron_installer/state.py
@@ -246,6 +246,7 @@ async def __instances_from_api__() -> dict[str, Instance]:
             https_proxy=working_host_entry.https_proxy,
             volttron_venv=working_host_entry.volttron_venv,
             volttron_home=working_host_entry.volttron_home,
+            host_configs_dir=working_host_entry.host_configs_dir if working_host_entry.host_configs_dir is not None else ""
         )
 
         instance = {
@@ -440,13 +441,22 @@ class PlatformPageState(rx.State):
         return self.connection_validity(working_platform)[1]["ansible_port"]
 
     @rx.var
-    def volttron_home_validity(self) -> bool:
+    def connection_volttron_home_validity(self) -> bool:
         if self.current_uid == "":
             return True
         working_platform: Instance | None = self.platforms.get(self.current_uid, None)
         if working_platform is None:
             return False
         return self.connection_validity(working_platform)[1]["volttron_home"]
+
+    @rx.var
+    def connection_host_configs_dir_validity(self) -> bool:
+        if self.current_uid == "":
+            return True
+        working_platform: Instance | None = self.platforms.get(self.current_uid, None)
+        if working_platform is None:
+            return False
+        return self.connection_validity(working_platform)[1]["host_configs_dir"]
 
     # ==== vars for platform validation ===
     @rx.var
@@ -776,6 +786,7 @@ class PlatformPageState(rx.State):
         host_request = working_platform.host.to_dict()
         host_request["ansible_port"] = int(host_request["ansible_port"])
         host_request["name"] =  working_platform.platform.config.instance_name
+        host_request["host_configs_dir"] = None if host_request["host_configs_dir"] == "" else host_request["host_configs_dir"]
         request = CreateOrUpdateHostEntryRequest(**host_request)
 
         await add_host(request)
@@ -901,7 +912,8 @@ class PlatformPageState(rx.State):
             "http_proxy" : True,
             "https_proxy" : True,
             "volttron_venv" : True,
-            "volttron_home" : True
+            "volttron_home" : True,
+            "host_configs_dir" : True
         }
         # Validate the host id
         if working_platform.host.id == "":
@@ -929,6 +941,12 @@ class PlatformPageState(rx.State):
             working_platform.advanced_expanded = True
             valid = False
             validity_map["volttron_home"] = False
+
+        # Validate host configs dir
+        if working_platform.host.host_configs_dir in [p.host.host_configs_dir for p in self.in_file_platforms if p.new_instance == False and p.host.id == working_platform.host.id and self.current_uid != p.platform.safe_platform["config"]["instance_name"]]:
+            working_platform.advanced_expanded = True
+            valid = False
+            validity_map["host_configs_dir"] = False
 
         return (valid, validity_map)
 
